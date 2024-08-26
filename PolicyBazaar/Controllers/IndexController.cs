afbf2dec6fa2d0c607f577ad8ae91f352e56cda9
sahil shaikh <sahilharshalv@gmail.com>
2024-08-27 01:31:55 +0530

    initial commit

---
+++ PolicyBazaar/Controllers/IndexController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PolicyBazaar.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
